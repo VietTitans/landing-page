f671a58915a023ddbd556fb843507fe46fc72f06
VietTitans <43970143+VietTitans@users.noreply.github.com>
2023-11-26 11:05:39 +0100

    Added dummy login logic

--- login.js
+++ login.js
@@ -1,4 +1,13 @@
-let username = prompt("");
-let password;
+let username = prompt("Hello! Please enter your name");
+let password = prompt("Please enter password");
 
-prompt("Hello")
+switch(inputPassword){
+    case "admin":
+        prompt("Hello master! Please enter password.");
+        break;
+    case ("" || e.key === "Escape"):
+        alert("Cancelled");
+        break;
+    default:
+        alert("I dont know you");
+}
